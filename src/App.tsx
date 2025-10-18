2a386da3a2d793f67cff65c05be9713c1c8c4872
labyrinthgarden <ochoasabogaldavid@gmail.com>
2025-10-17 23:29:16 -0500

    Se implementan las opciones para el cambio de color de los elementos

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,42 @@ import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [fontColor, setFontColor] = useState("#FFFFFF");
+  const [editorFontColor, setEditorFontColor] = useState("#FFFFFF");
+  const [bgColor, setBgColor] = useState("rgba(0,0,0,0.5)");
+  const [editorBgColor, setEditorBgColor] = useState("rgba(0,0,0,0.5)");
+
+  const updateCSSVariable = (variable: string, value: string) => {
+    document.documentElement.style.setProperty(variable, value);
+  };
+
+  const handleFontColorChange = (color: string) => {
+    setFontColor(color);
+    updateCSSVariable("--font-color", color);
+  };
+  const handleEditorFontColorChange = (color: string) => {
+    setEditorFontColor(color);
+    updateCSSVariable("--editor-font-color", color);
+  };
+  const handleBgColorChange = (color: string) => {
+    setBgColor(color);
+    updateCSSVariable("--bg-color", color);
+  };
+  const handleEditorBgColorChange = (color: string) => {
+    setEditorBgColor(color);
+    updateCSSVariable("--editor-background-color", color);
+  };
 
   return (
     <div className="app-container">
       <Titlebar title="Basic Text Editor" />
       <div className="main-container">
-      <Navbar/>
+        <Navbar
+          onFontColorChange={handleFontColorChange}
+          onEditorFontColorChange={handleEditorFontColorChange}
+          onBgColorChange={handleBgColorChange}
+          onEditorBgColorChange={handleEditorBgColorChange}
+        />
         <div className="filename">
           <h1>main.c</h1>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,66 @@
-function Navbar() {
+import { useState, useRef, useEffect } from 'react';
+
+type NavbarProps = {
+  onFontColorChange: (color: string) => void;
+  onEditorFontColorChange: (color: string) => void;
+  onBgColorChange: (color: string) => void;
+  onEditorBgColorChange: (color: string) => void;
+};
+
+function Navbar({
+  onFontColorChange,
+  onEditorFontColorChange,
+  onBgColorChange,
+  onEditorBgColorChange,
+}: NavbarProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-menu">
+    <nav className="navbar-container">
+      <div style={{ display: 'flex' }}>
         <button className="navbar-button">File</button>
         <button className="navbar-button">Edit</button>
         <button className="navbar-button">View</button>
+      </div>
+      <div style={{ position: 'relative', display: 'inline-block' }} ref={menuRef}>
+        <button className="navbar-button" onClick={toggleMenu}>...</button>
+        {isMenuOpen && (
+          <div className="dropdown-menu">
+            <div className='dropdown-button'>
+              <label>Font color:</label>
+              <input type="color" onChange={e => onFontColorChange(e.target.value)} />
+            </div>
+            <div className='dropdown-button'>
+              <label>Editor font color:</label>
+              <input type="color" onChange={e => onEditorFontColorChange(e.target.value)} />
+            </div>
+            <div className='dropdown-button'>
+              <label>Bg color:</label>
+              <input type="color" onChange={e => onBgColorChange(e.target.value)} />
+            </div>
+            <div className='dropdown-button'>
+              <label>Editor bg color:</label>
+              <input type="color" onChange={e => onEditorBgColorChange(e.target.value)} />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
